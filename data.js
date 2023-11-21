30c828bc6e1524ba4ed18a7a98f81c95cb697442
David Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2023-11-21 16:18:17 +0000

    Created array of objects containing blog posts.

---
+++ data.js
@@ -0,0 +1,38 @@
+export const postsData = [
+    {
+        image: `./images/article-image-01.png`,
+        date: `July 23, 2022`,
+        title: `Blog one`,
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    },
+    {
+        image: `./images/article-image-02.png`,
+        date: `July 23, 2022`,
+        title: `Blog two`,
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`, 
+    },
+    {
+        image: `./images/article-image-03.png`,
+        date: `July 23, 2022`,
+        title: `Blog three`,
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    },
+    {
+        image: `./images/blog-image-03.png`,
+        date: `July 23, 2022`,
+        title: `Blog four`,
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    },
+    {
+        image: `./images/blog-image-01.png`,
+        date: `July 23, 2022`,
+        title: `Blog five`,
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    },
+    {
+        image: `./images/blog-image-05.png`,
+        date: `July 23, 2022`,
+        title: `Blog six`,
+        text: `I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.`,
+    },
+];
